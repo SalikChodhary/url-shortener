7d7bc542469349d1178f82595907dcde481836cc
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2020-06-28 20:02:46 -0400

    creates short urls

---
+++ routes/url.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const validUrl = require("valid-url");
+const shortId = require("shortid");
+const config = require("config");
+
+const Url = require("../models/Url");
+
+// @route   POST /api/url/shorten
+// @desc    Create short url
+router.post("/shorten", (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = config.get("baseUrl");
+
+  if (!validUrl.isUri(baseUrl)) {
+    //console.log(baseUrl)
+    return res.status(401).json("Invalid base url");
+  }
+
+  const urlCode = shortId.generate();
+
+  if (validUrl.isUri(longUrl)) {
+    let url = Url.findOne({ longUrl });
+    url
+      .then((data) => {
+        //console.log(data);
+        if (data) {
+          console.log("url exists");
+          res.json(data);
+        } else {
+          const shortUrl = baseUrl + "/" + urlCode;
+
+          const newUrl = new Url({
+            longUrl,
+            shortUrl,
+            urlCode,
+            date: new Date(),
+          });
+          console.log("url does not exist");
+          newUrl.save().then(() => res.json(newUrl));
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json("Server Error");
+      });
+  } else {
+    res.status(401).json("Invalid long URL");
+  }
+});
+
+module.exports = router;
