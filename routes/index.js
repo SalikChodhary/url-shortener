1ee108d2fe9f02a23d9abab327fbfd144d3c40e8
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2020-06-30 23:22:45 -0400

    API setup and react init

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 const express = require('express')
 const router = express.Router();
 
+const Url = require('../models/Url')
+
+// @route   GET /:code
+// @desc    Redirect to long/original URL
+router.get('/:code', (req, res) => { 
+  Url.findOne({ urlCode: req.params.code })
+    .then((data) => {
+      console.log(req.params.code)
+      if(data) { 
+        return res.redirect(data.longUrl);
+      } else { 
+        return res.status(404).json('No Url found')
+      }
+    })
+    .catch(err => { 
+      console.error(err);
+      res.status(500).json('Server error');
+    })
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 
@@ -13,7 +14,14 @@ app.use(express.json({ extended: false }));
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
+if (process.env.NODE_ENV === 'production') { 
+  app.use(express.static('client/build'))
 
-const PORT = 5000; 
+  app.get('*', (req, res) => { 
+    res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+  })
+}
 
-app.listen(PORT, () => console.log("server running"))
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`))
